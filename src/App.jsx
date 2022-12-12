fb337ebe43f31e9b19f540859c23bb9c7ce32915
RKdoom <rd2020-2855@uce.edu.do>
2022-12-12 01:47:39 -0400

    navbarC visualizacion
    Visualizacion de navbarC

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import NavScrollExample from './Components/navbar';
+// import NavScrollExample from './Components/navbar';
 import Login from './Components/login';
 import Menu from './Components/menu';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
@@ -7,13 +7,14 @@ import { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
 import Mantenimiento from './Components/Mantenimiento';
 import Mecanico from './Components/Mecanico';
+import NavScrollExample from './Components/navbarC';
 
 
 function App()  {
   return (
    <>
-<Mecanico></Mecanico>
-
+{/* <Mecanico></Mecanico> */}
+<NavScrollExample/>
 {/* ---------------------- */}
    {/* <NavScrollExample/> */}
    {/* <Login/> */}
--- src/Components/navbarC.jsx
+++ src/Components/navbarC.jsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import BotonesMenu from './BotonesMenu';
+// import BotonesMenu from './BotonesMenu';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 
