8018501315fd42e741ab794cd0358994d0909547
Ingeep <101308545+Ingeep@users.noreply.github.com>
2022-12-16 00:44:38 -0400

    Update navbarC.jsx

--- src/Components/navbarC.jsx
+++ src/Components/navbarC.jsx
@@ -22,7 +22,7 @@ function NavScrollExample() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' , paddingLeft: '600px' }}
+            style={{ maxHeight: '100px' , paddingLeft: '920px' }}
             navbarScroll
           >
            <Nav.Link href="/Cliente">Cliente</Nav.Link>
